80ee5edad77bb0f1553e879571e8d69943168692
Jackietruong2805 <truonghao2805@gmail.com>
2024-03-01 10:55:22 +0700

    add 404 page

--- src/useRouteElement.tsx
+++ src/useRouteElement.tsx
@@ -1,13 +1,14 @@
 import { Navigate, Outlet, useRoutes } from 'react-router-dom'
-import ProductList from './pages/ProductList'
+// import ProductList from './pages/ProductList'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import RegisterLayout from './layouts/RegisterLayout'
 import MainLayout from './layouts/MainLayout'
 import Profile from './pages/Profile'
-import { useContext } from 'react'
+import { useContext, lazy, Suspense } from 'react'
 import { AppContext } from './context/app.context'
 import path from './constant/path'
+import NotFound from './pages/NotFound'
 
 function ProtectedRoute() {
   const { isAuthenticated } = useContext(AppContext)
@@ -19,6 +20,8 @@ function RejectedRoute() {
   return !isAuthenticated ? <Outlet /> : <Navigate to='/' />
 }
 
+const ProductList = lazy(() => import('./pages/ProductList'))
+
 export default function useRouteElement() {
   const routeElements = useRoutes([
     {
@@ -26,7 +29,9 @@ export default function useRouteElement() {
       index: true,
       element: (
         <MainLayout>
-          <ProductList />
+          <Suspense fallback={<div>Loading...</div>}>
+            <ProductList />
+          </Suspense>
         </MainLayout>
       )
     },
@@ -65,6 +70,10 @@ export default function useRouteElement() {
           )
         }
       ]
+    },
+    {
+      path: '*',
+      element: <NotFound />
     }
   ])
   return routeElements
